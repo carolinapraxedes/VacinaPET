de93ac5020f7910a211ae75c137d3b84e9791028
Carolina Praxedes <ana_carolina47@hotmail.com>
2023-08-31 21:52:08 -0300

    feat: aplicacao do select2

--- code/resources/views/layouts/app.blade.php
+++ code/resources/views/layouts/app.blade.php
@@ -20,8 +20,12 @@
     <!-- CSS Files -->
     <link id="pagestyle" href="{{ asset('assets/css/argon-dashboard.css') }}" rel="stylesheet" />
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"
+        integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
 
 
 </head>
@@ -33,24 +37,26 @@
     @endguest
 
     @auth
-        @if (in_array(request()->route()->getName(), [
-            'sign-in-static', 'sign-up-static', 'login', 'register', 'recover-password', 'rtl', 'virtual-reality'
-            ]))
+        @if (in_array(request()->route()->getName(),
+                ['sign-in-static', 'sign-up-static', 'login', 'register', 'recover-password', 'rtl', 'virtual-reality']))
             @yield('content')
         @else
-            @if (!in_array(request()->route()->getName(), ['profile', 'profile-static','vaccination.index']))
+            @if (
+                !in_array(request()->route()->getName(),
+                    ['profile', 'profile-static', 'vaccination.index']))
                 <div class="min-height-300 bg-info position-absolute w-100"></div>
-            @elseif (in_array(request()->route()->getName(), ['profile', 'profile-static','vaccination.index']))
-                <div class="position-absolute w-100 min-height-400 y-100 top-0" style="background-image: url('{{ asset('assets/img/teste.png') }}'); background-position: 30%;;">
+            @elseif (in_array(request()->route()->getName(),
+                    ['profile', 'profile-static', 'vaccination.index']))
+                <div class="position-absolute w-100 min-height-400 y-100 top-0"
+                    style="background-image: url('{{ asset('assets/img/teste.png') }}'); background-position: 30%;;">
                     <span class="mask bg-info opacity-6"></span>
                 </div>
             @endif
-            
-            @endif
-            @include('layouts.navbars.auth.sidenav') 
-                <main class="main-content border-radius-lg">
-                    @yield('content')
-                </main>
+        @endif
+        @include('layouts.navbars.auth.sidenav')
+        <main class="main-content border-radius-lg">
+            @yield('content')
+        </main>
     @endauth
 
     <!--   Core JS Files   -->
@@ -58,7 +64,7 @@
     <script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
     <script src="{{ asset('assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
     <script src="{{ asset('assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
-    
+
     <script>
         var win = navigator.platform.indexOf('Win') > -1;
         if (win && document.querySelector('#sidenav-scrollbar')) {
@@ -67,6 +73,10 @@
             }
             Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
         }
+
+        $(document).ready(function() {
+            $('.js-example-basic-single').select2();
+        });
     </script>
     <!-- Github buttons -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
--- code/resources/views/pages/appointment/create.blade.php
+++ code/resources/views/pages/appointment/create.blade.php
@@ -7,98 +7,61 @@
             
             <div class="card mb-4">
                 <div class="card-header pb-0">
-                    <h6>Cadastramento de Vacinação</h6>
+                    <h6>Agendamento de Vacinação</h6>
                 </div>
                 <div class="card-body">
                     <div class="row">
                         <div class="col-md-12">
                             <form role="form" method="POST" action={{ route('vaccination.store') }} enctype="multipart/form-data">
                                 @csrf
-                                <p class="text-uppercase text-sm">Informações sobre PET</p>
+                                <p class="text-uppercase text-sm">Informações sobre o agendamento</p>
                                 <div class="row">
-                                    <div class="col-md-4">
+                                    <div class="col-md-3">
                                         <div class="form-group">
-                                            <label for="example-text-input" class="form-control-label">Nome</label>
-                                            <input class="form-control" type="text" name="namePet"  value="" readonly>
+                                            <label for="example-text-input" class="form-control-label">RGA</label>
+                                            <input class="form-control" type="text" name="rgaAppointment"  value="{{ old('dateAppointment') }}">
                                             
-                                            @error('namePet') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
+                                            @error('rgaAppointment') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
                                             
                                         </div>
                                     </div>
-                                    <div class="col-md-5">
+                                    <div class="col-md-3">
                                         <div class="form-group">
-                                            <label for="example-text-input" class="form-control-label">Espécie</label>
-                                            <input class="form-control" type="text" name="speciePet" value="" readonly>
+                                            <label for="example-text-input" class="form-control-label">Data da vacinação</label>
+                                            <input class="form-control" type="date" name="dateAppointment"  value="{{ old('dateAppointment') }}">
+                                            
+                                            @error('dateAppointment') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
+                                            
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <label for="example-text-input" class="form-control-label">Horário</label>
+                                            <input class="form-control"  type="time" name="timeAppointment" value="{{ old('timeAppointment') }}" >
 
-                                            @error('speciePet') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
+                                            @error('timeAppointment') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <label for="example-text-input" class="form-control-label">Local da vacinação</label>
+                                            <select class="form-control js-example-basic-single"  name="localAppointment" id="localAppointment">
+
+                                                <option selected disabled>Selecione o local</option>
+                                                @foreach($locals as $local)
+                                                    <option value="{{ $local->id }}">{{ $local->name }}</option>
+                                                @endforeach
+                                            </select>
+
+                                            @error('localAppointment') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
                                         </div>
                                     </div>
                       
 
-                                    <div class="col-md-3">
-                                        <div class="form-group">
-                                            <label for="example-text-input" class="form-control-label">Sexo</label>
-                                            <input class="form-control" type="text" name="genderPet" value="" readonly>
-                                            @error('genderPet') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror                                         
-                                           
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="form-group">
-                                            <label for="example-text-input" class="form-control-label">Registro Geral Animal (RGA)</label>
-                                            <input class="form-control" type="text" name="rgaPet" value="" readonly>  
-                                            @error('rgaPet') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror                                            
-                                        </div>
-                                    </div>
-                                    <div class="col-md-3">
-                                        <div class="form-group">
-                                            <label for="example-text-input" class="form-control-label">Data de Nascimento</label>
-                                            <input class="form-control" type="text" name="dateBirthPet" id="dateBirthPet" value="" readonly>
-                                            @error('dateBirthPet') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror 
-                                        </div>
-                                    </div>
-                                    <div class="col-md-5">
-                                        <div class="form-group">
-                                            <label for="example-text-input" class="form-control-label">Nome do Tutor</label>
-                                            <input class="form-control" type="text" name="nameTutor"  value="" readonly>
-                                            @error('nameTutor') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
-                                            
-                                        </div>
-                                    </div>
-                                    <div class="col-md-3">
-                                        <div class="form-group">
-                                            <label for="example-text-input" class="form-control-label">CPF do Tutor</label>
-                                            <input class="form-control" type="text" name="cpfTutor"  value="" readonly>
-                                            @error('cpfTutor') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
-                                            
-                                        </div>
-                                    </div>
+                                   
                                 </div>                               
                                 <hr class="horizontal dark">
 
-                                    <div class="col-md-5">
-                                        <div class="form-group">
-                                            <label for="example-text-input" class="form-control-label">Profissional Aplicador</label>
-                                            <select class="form-control" name="veterinarian_id" id="veterinarian_id">
-                                                
-                                                <option selected disabled>Selecione o Veterinário</option>
-                                                @foreach($locals as $local)
-                                                    <option value="{{ $local->id }}">{{ $local->name }}</option>
-                                                @endforeach
-                                            </select>                                             
-                                            @error('veterinarian_id') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror 
-                                            
-                                        </div>
-                                    </div>
-                                    <div class="col-md-2">
-                                        <div class="form-group">
-                                            <label for="example-text-input" class="form-control-label">CRMV</label>
-                                            <input class="form-control" type="text" name="vaccinatorCRMV" placeholder="Digite o CRMV do profisional aqui..."  value="{{ old('vaccinatorCRMV') }}" >
-                                            @error('vaccinatorCRMV') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
-                                            
-                                        </div>
-                                    </div>
-                                
                                 <div class="row">
                                     <div class="d-flex align-items-center">                                                                      
                                         <button type="submit" class="btn btn-primary btn-sm ms-auto">Registrar</button>
